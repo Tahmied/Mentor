030c66d7f1fc6bfa48d513e3964f84ffe0b40480
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-11-12 13:16:06 +0600

    - fixed title issues

--- src/Components/Homepage/Home.jsx
+++ src/Components/Homepage/Home.jsx
@@ -1,4 +1,5 @@
 import { Suspense } from "react";
+import { Helmet } from 'react-helmet';
 import Loader from "../Utilis/Loader";
 import Choose from "./Choose";
 import Courses from "./Courses";
@@ -9,11 +10,14 @@ import Topinstructor from "./Topinstructor";
 const Home = () => {
     return (
         <>
+            <Helmet>
+                <title>Mentor | Learn anything from us</title>
+            </Helmet>
             <Hero></Hero>
             <Suspense fallback={<Loader></Loader>}>
                 <Courses></Courses>
             </Suspense>
-            
+
             <Choose></Choose>
             <Topinstructor></Topinstructor>
             <Cta></Cta>
