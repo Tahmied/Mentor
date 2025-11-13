c2e33a04678557ab7f2f6b7b17a297ccd0918866
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-11-13 17:18:22 +0600

    - added AOS animation on header and all the headings

--- src/Components/Homepage/Hero.jsx
+++ src/Components/Homepage/Hero.jsx
@@ -1,21 +1,26 @@
+import Aos from 'aos';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './hero.css';
 
 const Hero = () => {
+    useEffect(()=>{
+        Aos.init()
+    },[])
     return (
         <section className="hero-section">
             <div className="hero-container">
                 <div className="hero-content">
-                    <h1>
+                    <h1 data-aos-delay="0" data-aos-duration='1200' data-aos="fade-up" className="hero-big-text">
                         Up Your <span className="highlight">Skills</span> To<br />
                         Advance Your Career Path
                     </h1>
-                    <p>
+                    <p data-aos-delay="400" data-aos-duration='1200' data-aos="fade-up" className="hero-big-text">
                         Join <span className="learner-count">200,000+</span> learners and gain practical, career-boosting knowledge.
                     </p>
                     
-                    <Link to={'/courses'}>
-                        <button className="cta-button">Browse Courses</button>
+                    <Link className="hero-big-text" to={'/courses'}>
+                        <button data-aos-delay="700" data-aos-duration='1200' data-aos="fade-up" className="cta-button">Browse Courses</button>
                     </Link>
 
                     <div className="hero-image-container">
--- src/Components/Utilis/Headings.jsx
+++ src/Components/Utilis/Headings.jsx
@@ -1,11 +1,16 @@
+import Aos from 'aos';
+import { useEffect } from 'react';
 import './Headings.css';
 const Headings = ({ sectionName, Title, Desc }) => {
+    useEffect(()=>{
+        Aos.init()
+    },[])
     return (
         <section className="heading-section">
             <div className="heading-container">
-                <div className="heading-badge">{sectionName}</div>
-                <h2 className="heading-heading">{Title}</h2>
-                <p className="heading-description">
+                <div data-aos-offset="200"  data-aos-delay="0" data-aos-duration='1200' data-aos="fade-up" className="heading-badge">{sectionName}</div>
+                <h2 data-aos-offset="200"  data-aos-delay="400" data-aos-duration='1200' data-aos="fade-up" className="heading-heading">{Title}</h2>
+                <p  data-aos-offset="200" data-aos-delay="700" data-aos-duration='1200' data-aos="fade-up" className="heading-description">
                     {Desc}
                 </p>
             </div>
